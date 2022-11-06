46f7296c462d8fe3d3834bc28b4473c518fbd205
tangsang <488132230@qq.com>
2022-11-06 14:41:49 +0800

    20221105

---
+++ c51项目/lib/tangsanglib/c51tangsang.h
@@ -0,0 +1,29 @@
+/*
+ *该库为tangsang亲手打造，人工打造，比较粗糙，功能欠缺或瑕疵，仅提供学习参考 
+ *该库为tangsang亲手打造，人工打造，比较粗糙，功能欠缺或瑕疵，仅提供学习参考 
+ *该库为tangsang亲手打造，人工打造，比较粗糙，功能欠缺或瑕疵，仅提供学习参考 
+ */
+ 
+ 
+ 
+ 
+//用宏将无符号的类型简化 
+#define INT "unsigned int"
+#define CHAR "unsigned char"
+
+
+
+
+
+#ifndef  _C51TANGSANG_H_
+#define  _C51TANGSANG_H_
+
+
+extern void delay(unsigned int);
+extern void show_digital(unsigned int th,unsigned int digital);
+
+
+
+#endif
+
+
