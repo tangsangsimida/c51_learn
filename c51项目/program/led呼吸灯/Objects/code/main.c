347c7f42aee83d1d7931c9ee4834cc7ff0cae6b0
tangsang <488132230@qq.com>
2022-11-22 17:30:25 +0800

    led呼吸灯没有用定时器完成

--- c51项目/program/led呼吸灯/Objects/code/main.c
+++ c51项目/program/led呼吸灯/Objects/code/main.c
@@ -1,15 +1,59 @@
 #include <intrins.h>
 #include "c51tangsang.h"
 
+
+int ms=0;
+
 //led呼吸灯
 int main()
 {
-    int i=0;
+    time_0_us_init();
+//    time_0_case=0;
 	while(1)
 	{
-        P2=0;
-        delay(i+=100);
-        P2=0xFF;
+//		for(i=0;i<100;i++)
+//		{
+//            for(count=0;count<15;count++)
+//            {
+//                P2_0=0;
+//                delayus(i);
+//                P2_0=0xFF;
+//                delayus(100-i);
+//            }
+//		}
+//		for(i=100;i>0;i--)
+//		{
+//            for(count=0;count<15;count++)
+//            {
+//                P2_0=0;
+//                delayus(i);
+//                P2_0=0xFF;
+//                delayus(100-i);
+//            }
+//		}
+
+
+
+
 	}
 }
 
+void addnum() interrupt 1
+{
+    int i;
+    TL0 = 0xFF;		//设置定时初值
+    TH0 = 0xFF;		//设置定时初值
+	ms++;
+    for(i=0;i<5;i++)
+    {
+        if(ms<95)
+        {
+            P2_0=0;
+        }
+        else
+        {
+            P2_0=1;
+        }
+    }
+    ms%=100;
+}
