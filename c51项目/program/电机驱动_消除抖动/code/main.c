bb3c2fca6974a202b5665c37586ecb81a89b7d55
tangsang <488132230@qq.com>
2023-02-05 13:22:09 +0800

    geshihua

--- c51项目/program/电机驱动_消除抖动/code/main.c
+++ c51项目/program/电机驱动_消除抖动/code/main.c
@@ -2,39 +2,50 @@
 #include "LCD1602_b.h"
 #include <intrins.h>
 
-char counter=0,counpare=0,button=0;
-char before=0,after=0;
+char counter = 0, counpare = 0, button = 0;
+char before = 0, after = 0;
 
 void time_0_ms() interrupt 1
 {
-    TH0=64535/256;
-    TL0=64535%256;
+    TH0 = 64535 / 256;
+    TL0 = 64535 % 256;
     counter++;
-	counter%=100;
-    if(counter>=100-counpare/2 || counter <=(100+counpare/2)%100)
+    counter %= 100;
+    if (counter >= 100 - counpare / 2 || counter <= (100 + counpare / 2) % 100)
     {
-        P1_0=1;
+        P1_0 = 1;
     }
     else
     {
-        P1_0=0;
+        P1_0 = 0;
     }
-
 }
 
 int main()
 {
     LCD_Init();
     time_0_ms_init();
-	while(1)
-	{
-        LCD_ShowNum(1,2,button,1);
-        if(buttom_getstate_4())
-			button++;
-        button%=4;
-        if(button==1){counpare=35;}
-        if(button==2){counpare=70;}
-        if(button==3){counpare=100;}
-        if(button==0){counpare=0;}
-	}
+    while (1)
+    {
+        LCD_ShowNum(1, 2, button, 1);
+        if (buttom_getstate_4())
+            button++;
+        button %= 4;
+        if (button == 1)
+        {
+            counpare = 35;
+        }
+        if (button == 2)
+        {
+            counpare = 70;
+        }
+        if (button == 3)
+        {
+            counpare = 100;
+        }
+        if (button == 0)
+        {
+            counpare = 0;
+        }
+    }
 }
