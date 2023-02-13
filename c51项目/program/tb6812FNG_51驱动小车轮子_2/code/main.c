e38418f61697a0a444dcc890a2e025bbc411b483
tangsang <488132230@qq.com>
2023-02-13 14:45:07 +0800

    两个轮子驱动

--- c51项目/program/tb6812FNG_51驱动小车轮子_2/code/main.c
+++ c51项目/program/tb6812FNG_51驱动小车轮子_2/code/main.c
@@ -4,12 +4,18 @@
 void Delay(unsigned char);
 void Trolley_power(void);
 
-sbit PWM_left_behind = P3^0; //PWM
+sbit PWM_left_behind = P3^0; //PWM1
 sbit STBY_left = P3^1;		 //芯片工作状态
 sbit IN1_left_behind = P3^2; //IN1
 sbit IN2_left_behind = P3^3; //IN2
 
-unsigned char compare=15,conter=0;
+sbit PWM_right_behind = P2^0; //PWM2
+sbit STBY_right = P2^1;		 //芯片工作状态2
+sbit IN1_right_behind = P2^3; //IN1_2
+sbit IN2_right_behind = P2^2; //IN2_2
+
+
+unsigned char compare1=36,compare2=100,conter1=0,conter2=0;
 
 //pwm给0代表刹车
 
@@ -18,7 +24,11 @@ int main ()
 	//time_0_init()
 	time_0_ms_init();
 	IN2_left_behind = 0;
+	IN2_right_behind = 1;
+	PWM_right_behind=1;
+	PWM_left_behind=1;
 	STBY_left = 1;
+	STBY_right=1;
 	while(1)
 	{
 		//正转
@@ -48,20 +58,48 @@ int main ()
 }
 void Trolley_power(void) interrupt 1
 {
-	TH0=65435/256;//高八位数据     晶振12mhz，一个周期为1us，最大65535us，
-	TL0=65435%256;//低八位数据->64535 = 65535-1000; -> 1000us=1ms; 定时器单位为1ms
-	conter++;
-	conter%=100;
-	if(conter<compare)
+	TH0=64535/256;//高八位数据     晶振12mhz，一个周期为1us，最大65535us，
+	TL0=64535%256;//低八位数据->64535 = 65535-1000; -> 1000us=1ms; 定时器单位为1ms
+	conter1++;
+	conter1%=100;
+	conter2++;
+	conter2%=100;
+	if(conter1<compare1)
 	{
 		IN1_left_behind = 1;
-		P2=0;
+//		IN1_right_behind =0;
+		
+		
 	}
 	else
 	{
 		IN1_left_behind = 0;
-		P2=1;
+//		IN1_right_behind =1;
+		
 	}
+	if(conter2<compare2)
+	{
+//		IN1_left_behind = 1;
+		
+		IN1_right_behind =0;
+		
+	}
+	else
+	{
+//		IN1_left_behind = 0;
+		IN1_right_behind =1;
+		
+	}
+//	if(conter1<compare)
+//	{
+//		IN1_right_behind = 0;
+//		
+//	}
+//	else
+//	{
+//		IN1_right_behind = 1;
+//		
+//	}
 	
 }
 // ms
